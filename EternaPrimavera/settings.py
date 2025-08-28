5ce4503309b3979cb94e85c38caf776dce2f6d3c
alexahurtado08 <ahurtadod@eafit.edu.co>
2025-08-28 18:01:07 -0500

    Creación apps

--- EternaPrimavera/settings.py
+++ EternaPrimavera/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'carrito',
+    'usuarios', 
+    'productos',
+    'pedidos',
+    'reservas',
 ]
 
 MIDDLEWARE = [
