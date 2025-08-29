70c5ac3c33450e66fcb921a2595c2f912c4d522c
alexahurtado08 <ahurtadod@eafit.edu.co>
2025-08-29 18:59:44 -0500

    Añadir pedidos al admin

--- pedidos/admin.py
+++ pedidos/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Pedido, PedidoItem, Pago
 
-# Register your models here.
+
+
+class PedidoItemInline(admin.TabularInline):
+    model = PedidoItem
+    extra = 0
+    readonly_fields = ("subtotal",)
+
+
+class PagoInline(admin.StackedInline):
+    model = Pago
+    extra = 0
+    readonly_fields = ("fecha",)
+
+
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ("id", "usuario", "estado", "fecha", "total")
+    list_filter = ("estado", "fecha")
+    search_fields = ("usuario__username", "id")
+    inlines = [PedidoItemInline, PagoInline]
+
+
+
+@admin.register(PedidoItem)
+class PedidoItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "pedido", "producto", "cantidad", "precio", "subtotal")
+    list_filter = ("pedido",)
+    search_fields = ("producto", "pedido__id")
+
+
+
+@admin.register(Pago)
+class PagoAdmin(admin.ModelAdmin):
+    list_display = ("id", "pedido", "metodo", "estado", "fecha", "total")
+    list_filter = ("estado", "metodo", "fecha")
+    search_fields = ("pedido__id", "id")
