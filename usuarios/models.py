6e6f5a6f696bcc8bb5b08e6c20450f32b4ab50e9
Mariana Valderrama Castañeda <mvaderrc1@eafit.edu.co>
2025-08-28 21:04:27 -0500

    cracion de lista usuarios, registro, login

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+class UsuarioManager(BaseUserManager):
+    def create_user(self, correo, nombre, contrasena=None, **extra_fields):
+        if not correo:
+            raise ValueError("El usuario debe tener un correo electrónico")
+        correo = self.normalize_email(correo)
+        user = self.model(correo=correo, nombre=nombre, **extra_fields)
+        user.set_password(contrasena)  # se guarda en hash
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, correo, nombre, contrasena=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(correo, nombre, contrasena, **extra_fields)
+
+class Usuario(AbstractBaseUser, PermissionsMixin):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField(unique=True)
+    telefono = models.CharField(max_length=15, blank=True, null=True)
+    direccion = models.TextField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UsuarioManager()
+
+    USERNAME_FIELD = "correo"
+    REQUIRED_FIELDS = ["nombre"]
+
+    def __str__(self):
+        return f"{self.nombre} ({self.correo})"
